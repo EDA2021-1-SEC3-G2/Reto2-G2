6ceeba5bcc0a1fc7a00b5156ac4cb57a6615a148
Julian-Leon <e.leonc@uniandes.edu.co>
2021-04-13 23:38:44 -0500

    avance req 5

--- App/view.py
+++ App/view.py
@@ -91,8 +91,9 @@ def MostLikedVideos(mostliked):
     """
     videos con mas likes
     """
-    for element in range(1, lt.size(mostliked)+1):
+    for element in range(1, lt.size(mostliked)):
         video = lt.getElement(mostliked, element)
+        print("")
         print(video["title"]+"   "+video["channel_title"]+"   "+video["publish_time"]+"    "+video["views"]+"   "+video["likes"]+"    "+video["dislikes"]+"    "+video["tags"])
 
 
